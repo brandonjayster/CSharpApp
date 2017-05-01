c299570d89d6ee4a967ff8c1db8d2adaeb993e77
brandonjayster <jayster@protonmail.com>
2017-05-01 18:10:09 -0500

    Basic Hello World

---
+++ CSbasicapp.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSbasicapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lets get this out of the way lol
+            Console.WriteLine("Hello Brandon");
+            //Basic hello world app this simple tells the console to read the text you have between the ()
+            Console.ReadLine();
+        }
+    }
+}
