3f7c9c4575eca63ef371833e109719b109acb428
Brandon <jayster@protonmail.com>
2017-05-04 09:38:10 -0500

    Update FindOutAnyIP.cs

--- FindOutAnyIP.cs
+++ FindOutAnyIP.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net;
+using System.Net;//allows for socket programming
 namespace WhtsMyIP
 {
     public partial class Form1 : Form// .net form
